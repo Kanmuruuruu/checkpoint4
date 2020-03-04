fd5f26419f478a5d3600a0837414cf63db521643
Gautier Gueri <gautiergueri@gmail.com>
2020-03-04 12:30:20 +0100

    css fix et bon apétit

--- front/racketview/src/components/Rank/Rank.js
+++ front/racketview/src/components/Rank/Rank.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Player from "../Player/Player";
+import './Rank.css';
 
 const Rank = () => {
   return (
-    <div>
+    <div className="rank">
       <h1>Classement</h1>
+      <Player firstname={"jean"} lastname={"paul"} age={21} id={2} playerDouble_id={null} rateDouble={"P12"} rateSingle={"P12"}/>
       <Player firstname={"jean"} lastname={"paul"} age={21} id={2} playerDouble_id={null} rateDouble={"P12"} rateSingle={"P12"}/>
       <Player firstname={"jean"} lastname={"paul"} age={21} id={2} playerDouble_id={null} rateDouble={"P12"} rateSingle={"P12"}/>
       <Player firstname={"jean"} lastname={"paul"} age={21} id={2} playerDouble_id={null} rateDouble={"P12"} rateSingle={"P12"}/>
