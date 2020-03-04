0ae677b3f8ff6fdca919942e2abc482f3309dfea
Gautier Gueri <gautiergueri@gmail.com>
2020-03-04 11:51:52 +0100

    un peu de style navbar

--- front/racketview/src/components/Navbar/Navbar.js
+++ front/racketview/src/components/Navbar/Navbar.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <ul>
-        <li>
-          <NavLink to="/" activeClassName="active">
-            <img
-              src={require("../../assets/streamline-icon-badminton-shuttlecock-racquet@48 x48 (1).png")}
-              alt="Raquette de badminton"
-            />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName="active" to="/classement">Liste des joueurs</NavLink>
-        </li>
-      </ul>
-    </div>
+    <ul className="navbar">
+      <li>
+        <NavLink className="navlink"  to="/" activeClassName="active">
+          <img
+            src={require("../../assets/streamline-icon-badminton-shuttlecock-racquet@48 x48 (1).png")}
+            alt="Raquette de badminton"
+          />
+        </NavLink>
+      </li>
+      <li>
+        <NavLink className="navlink" activeClassName="active" to="/classement">
+          Liste des joueurs
+        </NavLink>
+      </li>
+    </ul>
   );
 };
 
